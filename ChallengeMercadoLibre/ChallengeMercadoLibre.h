b027b8726f267a39510b8aea65a68a8e6ee2b7fc
Jhean Carlos Pineros Diaz <jpineros@veritran.com>
2022-02-26 08:18:02 -0500

    Initial project

---
+++ ChallengeMercadoLibre/ChallengeMercadoLibre.h
@@ -0,0 +1,18 @@
+//
+//  ChallengeMercadoLibre.h
+//  ChallengeMercadoLibre
+//
+//  Created by jpineros on 26/02/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ChallengeMercadoLibre.
+FOUNDATION_EXPORT double ChallengeMercadoLibreVersionNumber;
+
+//! Project version string for ChallengeMercadoLibre.
+FOUNDATION_EXPORT const unsigned char ChallengeMercadoLibreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChallengeMercadoLibre/PublicHeader.h>
+
+
