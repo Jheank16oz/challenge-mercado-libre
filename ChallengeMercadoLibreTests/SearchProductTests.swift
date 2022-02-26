61cbd1658964b5a7db2bc96f0d80a37ffbc6f780
Jhean Carlos Pineros Diaz <jpineros@veritran.com>
2022-02-26 10:36:11 -0500

    Guarantee that we can search more than once

--- ChallengeMercadoLibreTests/SearchProductTests.swift
+++ ChallengeMercadoLibreTests/SearchProductTests.swift
@@ -29,6 +29,19 @@ class SearchProductTests:XCTestCase {
         XCTAssertEqual(client.requestedQuery, query)
     }
     
+    func test_searchTwice_requestsDataFromURLAndQueryTwice(){
+        let url = URL(string: "https://a-given-url.com")!
+        let query = "any query"
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.search(query: query)
+        sut.search(query: query)
+        
+        XCTAssertEqual(client.requestedURLs, [url, url])
+        XCTAssertEqual(client.requestedQueries, [query, query])
+        XCTAssertEqual(client.requestedQuery, query)
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://a-given-url.com")!) -> (sut:SearchProduct, client: HTTPClientSpy) {
@@ -40,10 +53,15 @@ class SearchProductTests:XCTestCase {
     private class HTTPClientSpy:HTTPClient {
         var requestedURL: URL?
         var requestedQuery:String?
+        var requestedURLs = [URL]()
+        var requestedQueries = [String]()
         
         func get(from url: URL, query: String) {
             requestedURL = url
             requestedQuery = query
+            requestedURLs.append(url)
+            requestedQueries .append(query)
+            
         }
 
     }
