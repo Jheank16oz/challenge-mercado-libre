387f27733081632bdc853c7c8e680a43382baed5
Jhean Carlos Pineros Diaz <jpineros@veritran.com>
2022-02-26 10:02:28 -0500

    Refactor test to remove duplication

--- ChallengeMercadoLibreTests/SearchProductTests.swift
+++ ChallengeMercadoLibreTests/SearchProductTests.swift
@@ -29,23 +29,12 @@ protocol HTTPClient {
     
 }
 
-class HTTPClientSpy:HTTPClient {
-    
-    func get(from url: URL, query: String) {
-        requestedURL = url
-        requestedQuery = query
-    }
-    
-    var requestedURL: URL?
-    var requestedQuery:String?
-}
+
 
 class SearchProductTests:XCTestCase {
     
     func test_init_doesNotRequestDataFromURLAndQuery(){
-        let url = URL(string: "https://a-given-url.com")!
-        let client = HTTPClientSpy()
-        _ = SearchProduct(url:url, client: client)
+        let (_, client) = makeSUT()
         
         XCTAssertNil(client.requestedURL)
         XCTAssertNil(client.requestedQuery)
@@ -54,8 +43,7 @@ class SearchProductTests:XCTestCase {
     func test_search_requestDataFromURLAndQuery(){
         let url = URL(string: "https://a-given-url.com")!
         let query = "any query"
-        let client = HTTPClientSpy()
-        let sut = SearchProduct(url: url, client: client)
+        let (sut, client) = makeSUT(url: url)
         
         sut.search(query: query)
         
@@ -63,5 +51,23 @@ class SearchProductTests:XCTestCase {
         XCTAssertEqual(client.requestedQuery, query)
     }
     
+    // MARK: - Helpers
+    
+    private func makeSUT(url: URL = URL(string: "https://a-given-url.com")!) -> (sut:SearchProduct, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = SearchProduct(url: url, client: client)
+        return (sut, client)
+    }
+    
+    private class HTTPClientSpy:HTTPClient {
+        var requestedURL: URL?
+        var requestedQuery:String?
+        
+        func get(from url: URL, query: String) {
+            requestedURL = url
+            requestedQuery = query
+        }
+
+    }
     
 }
