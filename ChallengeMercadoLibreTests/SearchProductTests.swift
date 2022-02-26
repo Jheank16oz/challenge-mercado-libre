cf3e9f5376ee8fad6b8e3dcd7041f50f04f0950c
Jhean Carlos Pineros Diaz <jpineros@veritran.com>
2022-02-26 09:34:16 -0500

    test search  and make HTTPClient a protocol to protect production code

--- ChallengeMercadoLibreTests/SearchProductTests.swift
+++ ChallengeMercadoLibreTests/SearchProductTests.swift
@@ -10,22 +10,52 @@ import XCTest
 @testable import ChallengeMercadoLibre
 
 class SearchProduct{
+    let client: HTTPClient
+    
+    init(client: HTTPClient){
+        self.client = client
+    }
+    
+    func search() {
+        client.get(from: URL(string: "https://a-url.com")!, query:"any query")
+    }
+}
+
+protocol HTTPClient {
+    
+    func get(from url: URL, query:String)
     
 }
 
-class HTTPClient {
+class HTTPClientSpy:HTTPClient {
+    
+    func get(from url: URL, query: String) {
+        requestedURL = url
+        requestedQuery = query
+    }
+    
     var requestedURL: URL?
-    var query:String?
+    var requestedQuery:String?
 }
 
 class SearchProductTests:XCTestCase {
     
     func test_init_doesNotRequestDataFromURLAndQuery(){
-        let client = HTTPClient()
-        _ = SearchProduct()
+        let client = HTTPClientSpy()
+        _ = SearchProduct(client: client)
         
         XCTAssertNil(client.requestedURL)
-        XCTAssertNil(client.query)
+        XCTAssertNil(client.requestedQuery)
+    }
+    
+    func test_search_requestDataFromURLAndQuery(){
+        let client = HTTPClientSpy()
+        let sut = SearchProduct(client: client)
+        
+        sut.search()
+        
+        XCTAssertNotNil(client.requestedURL)
+        XCTAssertNotNil(client.requestedQuery)
     }
     
     
