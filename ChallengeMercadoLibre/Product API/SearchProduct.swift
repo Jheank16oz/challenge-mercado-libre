f91e8f02d08e76841fa22c96821e70e4182a84ac
Jhean Carlos Pineros Diaz <jpineros@veritran.com>
2022-03-01 09:04:04 -0500

    Move mapping logic to a new type ´ProductItemsMapper´

--- ChallengeMercadoLibre/Product API/SearchProduct.swift
+++ ChallengeMercadoLibre/Product API/SearchProduct.swift
@@ -41,9 +41,10 @@ public final class SearchProduct{
         client.get(from: url, query:query) { result in
             switch result {
             case let .success(data, response):
-                if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data){
-                    completion(.success(root.results.map{ $0.item }))
-                }else {
+                do {
+                    let items = try ProductItemsMapper.map(data, response)
+                    completion(.success(items))
+                } catch {
                     completion(.failure(.invalidData))
                 }
             case .failure:
@@ -54,17 +55,28 @@ public final class SearchProduct{
     }
 }
 
-private struct Root:Decodable {
-    let results:[Item]
-}
+private class ProductItemsMapper {
+    
+    private struct Root:Decodable {
+        let results:[Item]
+    }
 
-private struct Item:Decodable {
+    private struct Item:Decodable {
+        
+        public let id:UUID
+        public let title: String
+        public let price: Int
+        
+        var item:ProductItem{
+            return ProductItem(id: id, title: title, price: price)
+        }
+    }
     
-    public let id:UUID
-    public let title: String
-    public let price: Int
-    
-    var item:ProductItem{
-        return ProductItem(id: id, title: title, price: price)
+    static func map(_ data:Data, _ response: HTTPURLResponse) throws -> [ProductItem]{
+        guard response.statusCode == 200 else {
+            throw SearchProduct.Error.invalidData
+        }
+        let root = try JSONDecoder().decode(Root.self, from: data)
+        return root.results.map { $0.item}
     }
 }
