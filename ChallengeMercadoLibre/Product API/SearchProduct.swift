68b9d9a202ad20973bb861cc114b27fbbd3fc22f
Jhean Carlos Pineros Diaz <jpineros@veritran.com>
2022-03-02 08:08:08 -0500

    Move  ProductItemsMapper to its own file

--- ChallengeMercadoLibre/Product API/SearchProduct.swift
+++ ChallengeMercadoLibre/Product API/SearchProduct.swift
@@ -43,30 +43,3 @@ public final class SearchProduct{
         }
     }
 }
-
-private class ProductItemsMapper {
-    
-    private struct Root:Decodable {
-        let results:[Item]
-    }
-
-    private struct Item:Decodable {
-        
-        public let id:UUID
-        public let title: String
-        public let price: Int
-        
-        var item:ProductItem{
-            return ProductItem(id: id, title: title, price: price)
-        }
-    }
-    static var OK_200: Int { return 200 }
-    
-    static func map(_ data:Data, _ response: HTTPURLResponse) throws -> [ProductItem]{
-        guard response.statusCode == OK_200 else {
-            throw SearchProduct.Error.invalidData
-        }
-        let root = try JSONDecoder().decode(Root.self, from: data)
-        return root.results.map { $0.item}
-    }
-}
