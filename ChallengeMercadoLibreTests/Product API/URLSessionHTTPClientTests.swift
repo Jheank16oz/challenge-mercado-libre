57e1a98867a355f3be03dffd3b5967da59a28538
Jhean Carlos Pineros Diaz <jpineros@veritran.com>
2022-03-03 08:39:52 -0500

    code warning improve

--- ChallengeMercadoLibreTests/Product API/URLSessionHTTPClientTests.swift
+++ ChallengeMercadoLibreTests/Product API/URLSessionHTTPClientTests.swift
@@ -134,7 +134,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     
     private func anyData() -> Data {
-        return Data(bytes: "any data".utf8)
+        return Data.init("any data".utf8)
     }
     
     private func anyNSError() -> NSError {
